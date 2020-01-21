d6c6903628d0b16da37af61620fa2f53e8ae21d2
Ronak99 <punase.ronak99@gmail.com>
2020-01-21 18:09:07 +0530

    Updates

---
+++ precache-manifest.9ac81ded935223a9b14d3897758f9b96.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "11c66aa4edf899b8f4c98cb41e3daae6",
+    "url": "/ReactJSFINAL/static/media/news3.11c66aa4.png"
+  },
+  {
+    "revision": "ca836dfdb5a32cf5d094",
+    "url": "/ReactJSFINAL/static/css/main.b42ec551.chunk.css"
+  },
+  {
+    "revision": "6eb918ac1060be0ea4e9",
+    "url": "/ReactJSFINAL/static/js/1.6eb918ac.chunk.js"
+  },
+  {
+    "revision": "a916659051f25d892e22",
+    "url": "/ReactJSFINAL/static/js/runtime~main.a9166590.js"
+  },
+  {
+    "revision": "86bbfcd0c0f214e79822b03df6b55901",
+    "url": "/ReactJSFINAL/static/media/girl2.86bbfcd0.png"
+  },
+  {
+    "revision": "817758deac1f3c925683b1ffefc20e2c",
+    "url": "/ReactJSFINAL/static/media/girl1.817758de.png"
+  },
+  {
+    "revision": "70ef88641bdd667a9a9910108c18584d",
+    "url": "/ReactJSFINAL/static/media/slider1.70ef8864.png"
+  },
+  {
+    "revision": "f8ca55222d807b5a4b23440b63b7ce10",
+    "url": "/ReactJSFINAL/static/media/prize.f8ca5522.png"
+  },
+  {
+    "revision": "1ca439e2a7f4a3e0f889effaf8921e02",
+    "url": "/ReactJSFINAL/static/media/15.1ca439e2.png"
+  },
+  {
+    "revision": "242d6c9b3d7ba8c41a63bb6a43505ee8",
+    "url": "/ReactJSFINAL/static/media/action.242d6c9b.png"
+  },
+  {
+    "revision": "ca836dfdb5a32cf5d094",
+    "url": "/ReactJSFINAL/static/js/main.ca836dfd.chunk.js"
+  },
+  {
+    "revision": "b2db2fb9ce5b8d1e22f753b55da38425",
+    "url": "/ReactJSFINAL/static/media/news1.b2db2fb9.png"
+  },
+  {
+    "revision": "b9ee44445e6f3ace1bafae6d57c18912",
+    "url": "/ReactJSFINAL/static/media/news2.b9ee4444.png"
+  },
+  {
+    "revision": "9f957d2233d164b94844db5ccfc35c8d",
+    "url": "/ReactJSFINAL/static/media/happy-kids.9f957d22.png"
+  },
+  {
+    "revision": "388d95f4e399f52b5a9c7278084206ac",
+    "url": "/ReactJSFINAL/static/media/discont.388d95f4.png"
+  },
+  {
+    "revision": "00e8c5a67b89c087fe221b9dc4348238",
+    "url": "/ReactJSFINAL/static/media/baby-step.00e8c5a6.png"
+  },
+  {
+    "revision": "c89583d6773a6f6afd7b95eaf6320df9",
+    "url": "/ReactJSFINAL/static/media/kids.c89583d6.png"
+  },
+  {
+    "revision": "3ba118b6db7208b74164031ae9ff3ca4",
+    "url": "/ReactJSFINAL/static/media/family.3ba118b6.png"
+  },
+  {
+    "revision": "477a24f71c56e39094261d0baf00136a",
+    "url": "/ReactJSFINAL/static/media/invalid_151.477a24f7.png"
+  },
+  {
+    "revision": "1b873f9d96e19967fb21834c1b46a3fc",
+    "url": "/ReactJSFINAL/static/media/logo-black.1b873f9d.png"
+  },
+  {
+    "revision": "6699fd0becf9726f94e057a1c879b1b7",
+    "url": "/ReactJSFINAL/index.html"
+  }
+];
